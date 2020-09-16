7382d545aef0143a8e4575b6bcfcd89bf777deba
Ahmad Tantowi <ahmadtantowi@outlook.com>
2020-09-16 19:53:10 +0700

    Add mail receiver from file

--- SendMail/Options/MailOption.cs
+++ SendMail/Options/MailOption.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace SendMail.Options
 {
@@ -7,5 +10,19 @@ namespace SendMail.Options
         public string Subject { get; set; }
         public string Body { get; set; }
         public bool IsBodyHtml { get; set; }
+        public string Receiver { get; set; }
+
+        public async Task<IEnumerable<string>> GetReceivers()
+        {
+            var receiverPath = Path.Combine(Directory.GetCurrentDirectory(), Receiver);
+            var mails = new List<string>();
+            var mail = default(string);
+
+            using var sr = new StreamReader(receiverPath);
+            while ((mail = await sr.ReadLineAsync()) != null)
+                mails.Add(mail);
+
+            return mails;
+        }
     }
 }
