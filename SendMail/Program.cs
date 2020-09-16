9cf32327e2d22ea458a2c5e3cf813313ee169729
Ahmad Tantowi <ahmadtantowi@outlook.com>
2020-09-16 16:10:20 +0700

    Initialize project

---
+++ SendMail/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SendMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
