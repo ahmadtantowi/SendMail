0e7865b64366316e1bd63e406b9cf44a87d09a7d
Ahmad Tantowi <ahmadtantowi@outlook.com>
2020-09-21 14:29:40 +0700

    Change GetReceivers method as extension

--- SendMail/Options/MailOption.cs
+++ SendMail/Options/MailOption.cs
@@ -11,21 +11,5 @@ namespace SendMail.Options
         public string Body { get; set; }
         public bool IsBodyHtml { get; set; }
         public string Receiver { get; set; }
-
-        public async Task<IEnumerable<string>> GetReceivers()
-        {
-            var receiverPath = Path.Combine(Directory.GetCurrentDirectory(), Receiver);
-            var mails = new List<string>();
-            var mail = default(string);
-
-            using var sr = new StreamReader(receiverPath);
-            while ((mail = await sr.ReadLineAsync()) != null)
-            {
-                if (!string.IsNullOrWhiteSpace(mail) && !mail.StartsWith('/'))
-                    mails.Add(mail);
-            }
-
-            return mails;
-        }
     }
 }
--- SendMail/SendMailExtension.cs
+++ SendMail/SendMailExtension.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using SendMail.Options;
 
 namespace SendMail
 {
@@ -11,6 +14,22 @@ namespace SendMail
             var regex = new Regex(@$"\{tokenizeStart}(\w+)\{tokenizeEnd}", RegexOptions.Compiled);
 
             return regex.Replace(source, match => variables[match.Groups[1].Value]);
+        }
+
+        public static async Task<IEnumerable<string>> GetReceivers(this MailOption mailOption)
+        {
+            var receiverPath = Path.Combine(Directory.GetCurrentDirectory(), mailOption?.Receiver ?? throw new ArgumentException("Receiver text file not provided."));
+            var mails = new List<string>();
+            var mail = default(string);
+
+            using var sr = new StreamReader(receiverPath);
+            while ((mail = await sr.ReadLineAsync()) != null)
+            {
+                if (!string.IsNullOrWhiteSpace(mail) && !mail.StartsWith('/'))
+                    mails.Add(mail);
+            }
+
+            return mails;
         }
     }
 }
